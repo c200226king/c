0a32e3cd939a08327ab67a9a64907d6def6a6e9d
kingfar <c200226king@gmail.com>
2026-03-19 10:35:00 +0800

    switch

---
+++ The Switch Statement/if_else.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int position = 2;
+
+    if (position == 1){
+        printf("Gold");
+    }
+    else if(position == 2){
+        printf("Siliver");
+    }
+    else if(position == 3){
+        printf("Bronze");
+    }
+    else {
+        printf("No medal");
+    }
+    return 0;
+}
